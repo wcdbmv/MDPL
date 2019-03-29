09d1a945e5ccd6bfaec17bc5f0062a959a640d22
Kerimov Ahmed <kerimov.dev@yandex.ru>
2019-03-29 14:52:24 +0300

    Change div to shr&and in print_uhex

--- lab06_07/puhex.asm
+++ lab06_07/puhex.asm
@@ -12,18 +12,20 @@ print_uhex	proc	near
 	mov	bp, sp
 
 	mov	ax, [bp + 0004h]
-	mov	bx, 0010h
-	mov	cx, 0000h
+	mov	bx, 0000h
+	mov	cx, 0004h
 
  _print_uhex_upload:
-	mov	dx, 0000h
-	div	bx
+	mov	dx, ax
+	and	dx, 000fh
 	push	dx
-	inc	cx
+	shr	ax, cl
+	inc	bx
 
 	cmp	ax, 0000h
 	jnz	_print_uhex_upload
 
+	mov	cx, bx
 	mov	bx, offset table
 
  _print_uhex_download:
